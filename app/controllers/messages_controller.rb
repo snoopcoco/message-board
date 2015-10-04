70c2af2e98b37603b99ceef82b5c90d1eb3c78c2
snoopcoco <snoop5572@gmail.com>
2015-10-04 11:45:58 +0000

    add age

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -34,7 +34,7 @@ end
 
 private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body, :age)
   end
   def set_message
     @message = Message.find(params[:id])
